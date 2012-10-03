f920d565e45d34bf7e76cf86982eb0667b7178b6
Yak <yak@stageit.com>
2012-10-02 18:30:58 -0700

    Added restaurant scaffold

---
+++ spec/factories/restaurants.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :restaurant do
+    name "MyString"
+    location "MyString"
+    telephone "MyString"
+  end
+end
