12a184740150fa6b9490e31e1b9f264fa28d62a8
Yak <yak@stageit.com>
2012-10-02 18:15:24 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RestaurantReview::Application.config.secret_token = '66e721c30387c2a78cd06a70faf1220b6faacff0cdaef36a9c36d30589dbdb3b215c4abb9cc1c0271e47c507fd50d2d141d486b428fc95f678b9f6f14544fbe0'
